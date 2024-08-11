bede265c7c07cc43dd79f85912327f8be0aa8b7c
zhangliang <710761875@qq.com>
2024-08-11 16:53:17 +0800

    更新数据库配置

--- configcenter-assemble/src/main/java/org/antframework/configcenter/Main.java
+++ configcenter-assemble/src/main/java/org/antframework/configcenter/Main.java
@@ -19,6 +19,7 @@ import org.springframework.boot.SpringApplication;
 public class Main {
     public static void main(String[] args) {
         Apps.setProfileIfAbsent("dev");
+        System.setProperty("app.home",".");
         SpringApplication.run(Main.class, args);
     }
 }
